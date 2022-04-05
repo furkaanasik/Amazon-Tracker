ed77892966c0cac22362390bc82949dba22e4d8f
furkaanasik <furkaanasik@gmail.com>
2022-04-05 21:08:42 +0300

    sending sms methods

--- Scraper.py
+++ Scraper.py
@@ -1,21 +1,80 @@
+import requests
+import Keys
+import os
+from twilio.rest import Client
+from bs4 import BeautifulSoup as Bs
+from dotenv import load_dotenv
+
+
 class Scrap:
-    def __init__(self, *, is_sms=False, is_email=False, product_url="", product_title="", product_price):
+    is_Track = True
+
+    def __init__(self, *, is_sms=False, is_email=False, product_url=""):
+        if is_email:
+            self.email = str(input("Please enter your email = "))
+        if is_sms:
+            self.phone_number = str(input("Please enter your phone number = "))
+
+        self.product_title = None
+        self.product_price = None
+        self.soup = None
+        self.tracker_price = 0
         self.is_email = is_email
         self.is_sms = is_sms
         self.product_url = product_url
-        self.product_title = product_title
-        self.product_price = product_price
+        # Request
+        self.request_url()
 
-    def __send_sms(self):
-        pass
+    # Send request link
+    def request_url(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/99.0.4844.82 Safari/537.36"}
+        page = requests.get(self.product_url, headers=headers)
+        soup = Bs(page.content, 'html.parser')
+        self.soup = soup
+        # Scrap product title and price
+        self.scrap_title_and_price()
 
-    def __send_email(self):
-        pass
+    # Scrap product title and price
+    def scrap_title_and_price(self):
+        # FIND TITLE and PRICE VARIABLE
+        self.product_title = self.soup.find(id="productTitle").text.strip()
+        self.product_price = int(self.soup.find("span", {"class": "a-offscreen"})
+                                 .text.replace(",", "").replace("TL", ""))
+        print("-" * len(self.product_title))
+        print("Product Name = ", self.product_title)
+        print("Product Price = ", self.product_price)
+        print("-" * len(self.product_title))
+        self.tracker_price = int(input("Please enter the price you want to track =  "))
 
+    # Check product price. if tracking price is bigger than product price send email or sms.
     def check_price_from_url(self):
-        pass
+        print("Checking price...")
+        if self.is_Track:
+            self.product_price = int(self.soup.find("span", {"class": "a-offscreen"})
+                                     .text.replace(",", "").replace("TL", ""))
+            if self.tracker_price > self.product_price:
+                if self.is_sms:
+                    self.send_sms()
 
+                if self.is_email:
+                    self.send_email()
 
+                self.is_Track = False
 
+    # Send sms
+    def send_sms(self):
+        load_dotenv()
 
+        client = Client(os.getenv("account_sid"), os.getenv("auth_token"))
+        client.messages.create(
+            body=f"{self.product_title} product has dropped below {self.tracker_price}!",
+            from_=os.getenv("twilio_number"),
+            to=self.phone_number
+        )
+        print("sms")
 
+    # Send email
+    def send_email(self):
+        print("mail")
--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-import requests
-from bs4 import BeautifulSoup as Bs
+import Scraper
 from Scraper import Scrap as Sc
 import time
 
 # VARIABLES
-URL = "https://www.amazon.com.tr/JBLC100SIUBLK-C100-JBL-Kulak-Kulakl%C4%B1k/dp/B01DEWVZ2C/ref=sr_1_5?__mk_tr_TR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3B359DH5STAEZ&keywords=kulakl%C4%B1k&qid=1648578903&sprefix=kulakl%C4%B1k%2Caps%2C122&sr=8-5"
-email = str(input("do you want a email yes[y] or no[n]= ")).lower() == "y" and True or False
-sms = str(input("do you want a sms yes[y] or no[n]= ")).lower() == "y" and True or False
-tracker_price = int(input("Tracker Price = "))
-
-# CONNECT URL and GET HTML CODE
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"}
-page = requests.get(URL, headers=headers)
-soup = Bs(page.content, 'html.parser')
-
-# FIND TITLE and PRICE VARIABLE
-product_title = soup.find(id="productTitle").text.strip()
-product_price = int(soup.find("span", {"class": "a-offscreen"}).text.replace(",", "").replace("TL", ""))
+URL = "https://www.amazon.com.tr/JBLC100SIUBLK-C100-JBL-Kulak-Kulakl%C4%B1k/dp/B01DEWVZ2C/ref=sr_1_5?__mk_tr_TR=%C3" \
+      "%85M%C3%85%C5%BD%C3%95%C3%91&crid=3B359DH5STAEZ&keywords=kulakl%C4%B1k&qid=1648578903&sprefix=kulakl%C4%B1k" \
+      "%2Caps%2C122&sr=8-5 "
+is_email = str(input("do you want a email yes[y] or no[n]= ")).lower() == "y" and True or False
+is_sms = str(input("do you want a sms yes[y] or no[n]= ")).lower() == "y" and True or False
 
 # Create Scraper
-track_products = Sc(is_sms=sms, is_email=email, product_url=URL, product_title=product_title, product_price=product_price)
+track_products = Sc(is_sms=is_sms, is_email=is_email, product_url=URL)
 
 # Check product price every 5 seconds
-while(True):
+while track_products.is_Track:
     track_products.check_price_from_url()
     time.sleep(5)
+
+print("end.")
+
